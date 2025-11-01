e097b2793a1f87c5605ea533f59aba9bba6a8ac2
beluga <rbelu@mail.ru>
2025-11-01 20:59:12 +0500

    Part two

--- PartOne/main.cpp
+++ PartOne/main.cpp
@@ -17,6 +17,10 @@ void Print(int* &ptr, int &size) {
 	std::cout << "\n";
 }
 
+void DeleteDynArray(int*& ptr) {
+	delete[]ptr;
+}
+
 void DeleteLastElement(int* &ptr, int &size) {
 	size--;
 	int* newarr = new int[size];
@@ -27,11 +31,68 @@ void DeleteLastElement(int* &ptr, int &size) {
 
 	std::swap(newarr, ptr);
 
-	delete[]newarr;
+	DeleteDynArray(newarr);
 }
 
-void DeleteDynArray(int* &ptr) {
-	delete[]ptr;
+void EnterIndex(int& index) {
+	std::cout << "Enter index -> ";
+	std::cin >> index;
+}
+
+void EnterNumbers( int& number) {
+	std::cout << "Enter number -> ";
+	std::cin >> number;
+}
+
+void CreateInputNewNumbers(int*& ptr, int& size) {
+	int index;
+	int number;
+
+	EnterIndex(index);
+	EnterNumbers(number);
+	
+	size++;
+
+	int* newarr = new int[size];
+
+	for (int i = 0, j = 0; i < size; i++) {
+		newarr[i] = ptr[j];
+
+		j++;
+
+		if (newarr[i] == newarr[index - 1]) {
+			newarr[index - 1] = number;
+			j--;
+		}
+	}
+
+	std::swap(newarr, ptr);
+
+	DeleteDynArray(newarr);
+}
+
+void DeleteNumIndex(int* &ptr, int &size) {
+	int index;
+
+	EnterIndex(index);
+
+	size--;
+
+	int* newarr = new int[size];
+
+	for (int i = 0, j = 0; i < size; i++) {
+		newarr[i] = ptr[j];
+
+		j++;
+
+		if (newarr[i] == newarr[index - 2]) {
+			j++;
+		}
+	}
+
+	std::swap(newarr, ptr);
+
+	DeleteDynArray(newarr);
 }
 
 int main() {
@@ -45,6 +106,14 @@ int main() {
 
 	InizDinMemory(arr, size);
 	
+	Print(arr, size);
+
+	CreateInputNewNumbers(arr, size);
+
+	Print(arr, size);
+
+	DeleteNumIndex(arr, size);
+
 	Print(arr, size);
 
 	DeleteLastElement(arr, size);
